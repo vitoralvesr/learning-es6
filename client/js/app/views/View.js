130f5e212dcb86d8e3607b984c636ebba285b37f
Vitor Alves Rocha <vitoralvesrocha16@gmail.com>
2018-03-02 19:28:22 +0000

    Finalizando primeiro módulo - trabalhando com herança

---
+++ client/js/app/views/View.js
@@ -0,0 +1,14 @@
+class View {
+    
+    constructor(elemento) {
+        this._elemento = elemento;
+    }
+    
+    template() {
+        throw new Error('O método _template deve ser implementado!');
+    }
+    
+    update(model) {
+        this._elemento.innerHTML = this.template(model);
+    }
+}
